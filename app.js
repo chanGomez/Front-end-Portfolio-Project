4c693694a47bca92984b76a6739bd14b1133b7a4
Chantal <chantalgomez@pursuit.org>
2023-04-02 21:00:47 -0400

    Both html pag e are working. Go back button funtional. Info is formated.

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-//api === http://www.omdbapi.com/?t=fight+club&apikey=10fe5b60
+//store api in local storage
+localStorage.setItem("key", "10fe5b60");
 
+//add event listener to form
 const form = document.querySelector("form")
 form.addEventListener("submit", (event)=>{
     event.preventDefault();
+
+    // //remove intro paragraph
+    const introP = document.querySelector(".introText")
+    introP.remove()
+    //place footer down 
+    const footer = document.querySelector("div")
+    footer.style.position = `auto`;
+
     //get value of input and adjust it for instead of spaces it's a plus sign
     let input = event.target.textInput.value
-    //check if has space or not
+
+    //check if input in search bar has space or not
     function whiteSpaceOrNot(){
         let index = input.indexOf(' ') >= 0;
         if(index === true){
@@ -14,64 +25,36 @@ form.addEventListener("submit", (event)=>{
         }else{
             return input
         }}
+
     // fetch data
-    let api = `http://www.omdbapi.com/?t=` + whiteSpaceOrNot(input) + `&apikey=10fe5b60`
+    let api = `http://www.omdbapi.com/?t=` + whiteSpaceOrNot(input) + `&apikey=${localStorage.getItem("key")}`
 
         fetch(api)
         .then((response)=> response.json())
         .then((result)=>{
+
             //make main section show up and style it
             const main = document.querySelector("main")
-            document.querySelector("main").style.width = "70%"
-            document.querySelector("main").style.height = "700px"
+            document.querySelector("main").style.width = "60%"
             document.querySelector("main").style.justifyContent = "center"
-            document.querySelector("main").style.margin = "30px auto 0 auto"
-            document.querySelector("main").style.color = "color"
             document.querySelector("main").style.backgroundColor = "#cac7ff"
+            
             //img of poster
-            const imgContainer = document.querySelector(".imgContainer")
-            document.querySelector(".imgContainer").style.width = "300px" 
-            const img = document.createElement("img")
+            const imgPoster = document.createElement("img")
+            imgPoster.className = "imgPoster"
+            imgPoster.setAttribute(`src`, `${result.Poster}`)
+            imgPoster.style.width = "500px"
+            imgPoster.style.height = "auto"
+            main.prepend(imgPoster)
 
-            // img.setAttribute('src', '${result.poster}')
-
-            //movie info
-            const article = document.querySelector("article")
-            const h2 = document.createElement("h2")
-            h2.innerHTML = `<h2><strong> Title: </strong> ${result.Title}</h2>`
-            const p = document.createElement("p")
-            p.innerHTML = `<p><strong>Year:</strong> ${result.Year}<br/>
-            <strong>Genre:</strong> ${result.Genre} <br/>
-            <strong>Plot:</strong> ${result.Plot} </p>`
-
-            article.append(h2)
-            article.append(p)
-
-            console.log(result);
+            //more info about movie link to second html
+            const moreInfo = document.createElement("a")
+            moreInfo.setAttribute(`href`, `info.html?id=${result.imdbID}`)
+            moreInfo.textContent = `More Info`
+            moreInfo.className = "moreInfo"
+            imgPoster.after(moreInfo)
+            console.log(result); 
         })
-
-        
-
     form.reset()
 })
 
-// main{
-//     width: 70%;
-//     height: 700px;
-//     justify-content: center;
-//     margin: 0 auto 0 auto;
-//     margin-top: 30px;
-//     color: aliceblue;
-//     background-color: #cac7ff;
-    
-
-
-// }
-// section{
-//     grid-template-columns: 1/2;
-//     display: block;
-    
-// }
-// article{
-//     grid-template-columns: 2/3;  
-// }
---
+++ info.js
@@ -0,0 +1,68 @@
+//Add event.listener to link for second html
+window.addEventListener("load", (event) => {
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.get(`id`)
+
+        //http://www.omdbapi.com/?i=tt0317219
+        let api = `http://www.omdbapi.com/?i=` +  urlParams.get(`id`) + `&apikey=${localStorage.getItem("key")}`
+
+
+    fetch(api)
+    .then((response)=> response.json())
+    .then((result)=>{
+
+            const mainInfo = document.querySelector(".mainInfo")
+            const information = document.querySelector(".information")
+            const titleH2 = document.createElement("h2")
+            titleH2.textContent = `${result.Title}`
+            information.append(titleH2)
+            const pInfo = document.createElement("p")
+            pInfo.innerHTML = `<strong> Year: </strong>${result.Year}` + `<br>` + 
+            `<strong> Director: </strong>${result.Director}`  + `<br>`+
+            `<strong> Actors: </strong>${result.Actors}` + `<br>` +
+            `<strong> Genre: </strong>${result.Genre}` + `<br>` +
+            `<strong> Rated: </strong>${result.Rated}` + `<br>` +
+            `<strong> Plot: </strong>${result.Plot}`
+            information.append(pInfo)
+
+
+            //back button
+            const backButton = document.createElement("button")
+            backButton.textContent = "Back"
+            backButton.className = "goBackButton"
+            mainInfo.append(backButton)
+            backButton.style.fontSize = "30px";
+            backButton.style.color = "rgb(53, 14, 96)";
+            backButton.style.textDecoration = "none";
+            backButton.style.backgroundColor = "#cac7ff"
+
+
+            // box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+            // font-size: 30px;
+            // color: rgb(53, 14, 96);
+            // text-decoration: none;
+
+            //make button work
+            // backButton.setAttribute = (`onclick`,`history.back()`)
+            document.querySelector(".goBackButton").addEventListener("click", () => {
+                history.back();
+              });
+
+
+            const imgPoster = document.createElement("img")
+            imgPoster.className = "imgPoster"
+            imgPoster.setAttribute(`src`, `${result.Poster}`)
+            imgPoster.style.width = "270px"
+            imgPoster.style.height = "auto"
+            mainInfo.prepend(imgPoster)
+
+        console.log(result)
+
+    })
+
+    
+//if location.search has id then get info from id
+console.log(urlParams);
+console.log(urlParams.get(`id`));
+
+});
